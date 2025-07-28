0e3ba7365045b3b94b91449c82809005b96b610d
shaylasamara <samarashayla@gmail.com>
2025-07-28 07:20:52 +0600

    54545

--- frontend_sayhello_app/lib/main.dart
+++ frontend_sayhello_app/lib/main.dart
@@ -13,9 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
+
+        // THIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
         //
